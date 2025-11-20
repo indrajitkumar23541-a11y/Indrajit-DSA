76c1c4f7d516cd790441313447f248bbb30259f7
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-21 05:19:58 +0530

    Takin Input Simple Intrest Program

---
+++ Taking_Input_Simple_Intrest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float P;
+    cout<<"Enter P : ";
+    cin>>P;
+    float R;
+    cout<<"Enter R : ";
+    cin>>R;
+    float T;
+    cout<<"Enter T : ";
+    cin>>T;
+    cout<<"Simple Intrest = "<<(P*R*T)/100<<endl;
+
+}
