16b4097593d659ca9d6734bf7cab190b95785831
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-20 21:14:50 +0530

    Taking input Arithmetic Operators Float Data types program

---
+++ Taking_Input_Arithmetic_Operators_Float_DataTypes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x;
+    cout<<"Enter 1st Number : ";
+    cin>>x;
+    float y;
+    cout<<"Enter 2nd Number : ";
+    cin>>y;
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl;
+    return 0;
+
+}
