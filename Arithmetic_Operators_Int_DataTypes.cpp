a623d3568a89070e3600cd24b07036c178138d21
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-20 20:20:13 +0530

    Arithmetic Operators int data types program

---
+++ Arithmetic_Operators_Int_DataTypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 34;
+    int y = 17;
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl;
+    return 0;
+
+}
