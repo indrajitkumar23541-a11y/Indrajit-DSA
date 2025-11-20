7c2589fdad5c318e94375ca9ac047e1b27e4f841
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-20 20:27:47 +0530

    Arithmetic Operators Float data types program

---
+++ Arithmetic_Operators_Float_DataTypes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x = 120;
+    float y = 45;
+    cout<<x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y<<endl;
+    return 0;
+}
