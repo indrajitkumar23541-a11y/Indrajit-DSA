c746a63e23fa94403d0e3b1f1753228b9489dc94
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-21 05:36:04 +0530

    Takin Input Calulate Area Of circle Program

---
+++ Takin_Input_Area_Of_Circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float PI = 3.14;
+    float r;
+    cout<<"Enter Radius : ";
+    cin>>r;
+    cout<<"Area Of Circlr = "<<PI*r*r<<endl;
+    return 0;
+}
