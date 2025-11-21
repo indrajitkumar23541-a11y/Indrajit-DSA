5837f4e28d177a706c616410d03f6252be10f577
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-21 10:21:03 +0530

    Divisible by 5 and 3 Program

---
+++ Divisible_By_5and3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    if(n%5==0 && n%3==0){
+        cout<<"Divisible by 5 or 3";
+    }
+    else{
+        cout<<"Not Divisible by 5 or 3";
+    }
+    return 0;
+}
