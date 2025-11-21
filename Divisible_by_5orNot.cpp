270fbe3938b695cc057f419dc7dee48e1ea95448
indrajitkumar23541-a11y <indrajitkumar23541@gmail.com>
2025-11-21 09:08:50 +0530

    Divisible by 5 or not program

---
+++ Divisible_by_5orNot.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number : ";
+    cin>>n;
+    if(n%5==0){
+        cout<<"Divisible By 5";
+    }
+    else{
+            cout<<"Not Divisible by 5";
+        }
+        return 0;
+}
